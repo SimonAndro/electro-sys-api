47e733e3047274647e8438bb4d61679bd0c9324c
SimonAndro <41785978+SimonAndro@users.noreply.github.com>
2021-12-30 15:22:17 +0800

    add config example

---
+++ v1/config-example.php
@@ -0,0 +1,16 @@
+<?php
+return [
+    'db_host' => '',
+
+    'db_username' => '',
+
+    'db_name' => '',
+
+    'db_password' => '',
+
+    'key' => 'xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxx',
+        
+    'permalink' => true,
+
+    'debug' => true,
+];
